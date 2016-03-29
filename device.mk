d6a0e586f158300a1020313d87785edb1d2ccac0
doc HD <doc.divxm@gmail.com>
2016-03-29 08:54:27 +0300

    Angler: Fix data drops on some devices
    ** according to: http://review.cyanogenmod.org/#/c/132076/
    
    and http://gerrit.aicp-rom.com/22795 respectively
    
    Change-Id: Ib45a6e8f83101372f830f32e734df11bee395895
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -272,6 +272,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # LTE, CDMA, GSM/WCDMA
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=10 \
+    ro.telephony.ril.config=setPrefNwTypeOnUnsolConnected \
     telephony.lteOnCdmaDevice=1 \
     persist.radio.mode_pref_nv10=1 \
     ro.telephony.get_imsi_from_sim=true \
